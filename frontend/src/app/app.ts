695e897953dbbca876f8d43383728207d9657a74
imenmanou-5 <203617210+imenmanou-5@users.noreply.github.com>
2026-04-24 10:00:00 +0700

    feat: ajout routing et configuration Angular

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
+})
+export class App {
+  title = 'achat-fournisseur-frontend';
+}
